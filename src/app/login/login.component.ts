458b1fbc39b8614074a9348b59f82a152fbaedd3
sreematta <matta.s@husky.neu.edu>
2018-06-18 14:14:38 -0400

    added login controller

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  emailId;
+  passwordValue;
+  data;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  addCourse(email, password) {
+    this.data = email + password;
   }
 
 }
